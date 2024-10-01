1f37ef7bde66029542c3f9a61f1a63fd1a34fdd3
fsociety781 <raihanmuhammadsaprudin898@gmail.com>
2024-10-01 23:34:05 +0700

    Add MySQL support and define Prisma models for User, Device, Sensor, and related entities

---
+++ prisma/migrations/20241001163113_migration/migration.sql
@@ -0,0 +1,199 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `address` VARCHAR(100) NOT NULL,
+    `username` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `barnId` BIGINT NOT NULL,
+    `role` ENUM('admin', 'user') NOT NULL DEFAULT 'user',
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    INDEX `User_barn_id_fkey`(`barnId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `device` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `guid` VARCHAR(50) NOT NULL,
+    `versionId` BIGINT NOT NULL,
+    `barnId` BIGINT NOT NULL,
+    `status` ENUM('online', 'offline') NOT NULL DEFAULT 'offline',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `device_guid_key`(`guid`),
+    INDEX `device_barnId_fkey`(`barnId`),
+    INDEX `device_versionId_fkey`(`versionId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sensor` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `temperature` DOUBLE NOT NULL,
+    `humidity` DOUBLE NOT NULL,
+    `time` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `sensor_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `setDevice` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `MinS` INTEGER NOT NULL,
+    `MidS` INTEGER NOT NULL,
+    `MinK` INTEGER NOT NULL,
+    `MidK` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `setDevice_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `setSchedule` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `hour` INTEGER NOT NULL,
+    `minute` INTEGER NOT NULL,
+    `min_timer` INTEGER NOT NULL,
+    `second_timer` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `setSchedule_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `mode` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `mode` ENUM('manual', 'auto', 'haybrid') NOT NULL DEFAULT 'manual',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `mode_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `scheduleMode` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `schedule` ENUM('off', 'modeSchedule1', 'modeSchedule2') NOT NULL DEFAULT 'off',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `scheduleMode_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `history` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `mode` VARCHAR(50) NOT NULL,
+    `fan` VARCHAR(50) NOT NULL,
+    `pump` VARCHAR(50) NOT NULL,
+    `time` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `history_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `controll` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `deviceId` BIGINT NOT NULL,
+    `fan` INTEGER NOT NULL,
+    `pump` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `controll_deviceId_fkey`(`deviceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `barn` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `version` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `version` VARCHAR(50) NOT NULL,
+    `firmware` LONGBLOB NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `informatian` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `barnId` BIGINT NOT NULL,
+    `cycle` VARCHAR(50) NOT NULL,
+    `harvest_date` DATETIME(3) NOT NULL,
+    `harvest_yield` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `informatian_barnId_fkey`(`barnId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_barn_id_fkey` FOREIGN KEY (`barnId`) REFERENCES `barn`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `device` ADD CONSTRAINT `Device_barn_id_fkey` FOREIGN KEY (`barnId`) REFERENCES `barn`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `device` ADD CONSTRAINT `Device_version_id_fkey` FOREIGN KEY (`versionId`) REFERENCES `version`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `sensor` ADD CONSTRAINT `sensor_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `setDevice` ADD CONSTRAINT `setDevice_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `setSchedule` ADD CONSTRAINT `setSchedule_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `mode` ADD CONSTRAINT `mode_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `scheduleMode` ADD CONSTRAINT `scheduleMode_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `history` ADD CONSTRAINT `history_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `controll` ADD CONSTRAINT `controll_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `informatian` ADD CONSTRAINT `informatian_barnId_fkey` FOREIGN KEY (`barnId`) REFERENCES `barn`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
