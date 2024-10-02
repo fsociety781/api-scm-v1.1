2a9c1658adcebeb054569d4116e72b418870df1d
fsociety781 <raihanmuhammadsaprudin898@gmail.com>
2024-10-02 13:57:45 +0700

    Refactor database schema: change BigInt to Int for various models and make barnId optional in User model

--- prisma/migrations/20241002045925_make_barn_id_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `user` DROP FOREIGN KEY `User_barn_id_fkey`;
-
--- AlterTable
-ALTER TABLE `user` MODIFY `barnId` BIGINT NULL;
-
--- AddForeignKey
-ALTER TABLE `User` ADD CONSTRAINT `User_barn_id_fkey` FOREIGN KEY (`barnId`) REFERENCES `barn`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
